f008fe540053b51168745212be8b008b8aeb1be0
Shaahid <mohammadshaahid05@gmail.com>
2026-01-02 19:15:50 +0530

    Move String_Methods

---
+++ JavaScript/String_Methods/Pra.js
@@ -0,0 +1,30 @@
+// a=prompt('Enter a sentence').trim();
+// let count=1
+// for (let i = 0; i < a.length; i++) {
+//     if(a[i]===' '){
+//         count+=1
+//     }   
+// }
+// console.log('Number of words are: ',count);
+
+
+let a = [
+    { title: "Chicken Biryani", price: "250", rating: "⭐⭐⭐⭐", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv1W5xqVptOMLDVIru4DymVlOBN5SAvn7Vh5ikSote1eZ-oIYdWRvbRMnwIoTrqQ45XZPDf_cV736jlpdLPnjTmXkBgE527nU-1bOFKN54&s=10" },
+    { title: "FiredRice", price: "70", rating: "⭐⭐⭐⭐", img: "https://plus.unsplash.com/premium_photo-1673108852141-e8c3c22a4a22?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: "Maggi", price: "50", rating: "⭐⭐⭐⭐", img: "https://images.unsplash.com/photo-1551326844-4df70f78d0e9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2QlMjBpbWFnZXN8ZW58MHx8MHx8fDA%3D" },
+    { title: " Mutton Biryani", price: "300", rating: "⭐⭐⭐⭐", img: "https://images.unsplash.com/photo-1552611052-33e04de081de?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vZCUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D" },
+    { title: "Chicken Majestic", price: "275", rating: "⭐⭐⭐⭐", img: "https://plus.unsplash.com/premium_photo-1673590981810-894dadc93a6d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Zm9vZCUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D" },
+]
+
+for (let i = 0; i < a.length; i++) {
+    document.body.innerHTML += `
+        <div class="parent">
+        <div><img src="${a[i].img}" alt="" class="image"></div>
+        <div class="card">
+        <h1>${a[i].title}</h1>
+        <p>${a[i].price}</p>
+        <h2>${a[i].rating}</h2>
+        </div>
+        </div>
+    `
+}
---
+++ JavaScript/String_Methods/index.js
@@ -0,0 +1,66 @@
+// 1st
+// let a=prompt("Enter in between 10 characters").trim();
+// if(a.length === 0){
+//     console.log("Please enter something");
+// }
+// else if(a.length <= 10){
+//     console.log(a.length);
+// }
+// else{
+//     console.log("You have exceeded the limit for number of characters");
+// }
+
+
+//2nd
+// let a=prompt("Enter your name").trim();
+// a=a.charAt(0).toUpperCase() + a.slice(1).toLowerCase()
+// console.log(a);
+
+
+//3rd
+// let invite=["Shaahid", "Siva", "Uday", "Pawan", "Jahnavi"]
+// let found=false;
+// a=prompt("Enter your name").trim();
+// str = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase()
+// for (let index = 0; index < invite.length; index++) {
+//     if(invite[index] == str){
+//         found=true;
+//         break;
+//     }
+// }
+// if (found){
+//     console.log("You are invited to the party"); 
+// }
+// else{
+//     console.log("Get out from my mother land");
+// }
+
+
+//3rd(Alternative)
+// let invite=["Shaahid", "Siva", "Uday", "Pawan", "Jahnavi"]
+// a=prompt("Enter your name").trim();
+// a=a.charAt(0).toUpperCase()+a.slice(1).toLowerCase()
+// if(invite.includes(a)){
+//     console.log("You are invited to the party");
+// }
+// else{
+//     console.log("Get out from my mother land");
+// }
+
+
+// 4th
+// a=prompt("Enter the color")
+// document.body.style.backgroundColor = a;
+
+
+// 5th
+// a=prompt("Enter your name, age, favorite actor").trim();
+// document.body.style.width
+
+
+
+
+
+
+
+
