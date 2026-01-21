18c4fdb01306f9483c275ae6693b5fe22746b9df
Shaahid <mohammadshaahid05@gmail.com>
2026-01-21 22:49:33 +0530

    Move Practice

---
+++ JavaScript/Practice/app1.js
@@ -0,0 +1,57 @@
+// a ="This is a bad day"
+// a=a.replace("bad","good")
+// console.log(a);
+
+// a="frontend"
+// b=a.includes("end")
+// console.log(b);
+
+// a="JavaScript"
+// b=a.slice(4,10)
+// console.log(b);
+
+// a='apple,banana,orange'
+// b=a.split(',')
+// console.log(b);
+
+// a='hello'
+// b=a.split('').reverse().join('')
+// console.log(b);
+
+// a=["apple","banana","mango"]
+// b=a.includes("mango")
+// console.log(b);
+
+// a=[1,2]
+// b=[3,4]
+// c=a.concat(b)
+// console.log(c);
+
+// a=[5,1,4,2]
+// b=a.sort()
+// console.log(b);
+
+// a=[5,1,4,2,7,100,23,45,0]
+// for (let i = 0; i < a.length; i++) {
+//     for (let j = 0; j < a.length-1; j++) {
+//         if(a[j]>a[j+1]){
+//             let temp;
+//             temp=a[j]
+//             a[j]=a[j+1]
+//             a[j+1]=temp
+//         }
+        
+//     }
+    
+// }
+// console.log(a);
+
+// a=45
+// b=11
+// a=a+b //56
+// b=a-b //45
+// a=a-b
+
+let a=[10,20,30,40]
+a.forEach(value => console.log(value))
+
