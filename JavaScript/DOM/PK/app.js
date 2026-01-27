e997ae5408d2a88e91f65ebbb4e7667553fbb958
Shaahid <mohammadshaahid05@gmail.com>
2026-01-27 17:05:42 +0530

    PK

---
+++ JavaScript/DOM/PK/app.js
@@ -0,0 +1,26 @@
+
+//Timer
+// function time() {
+//     text = document.getElementById('text1')
+//     wait = document.getElementById('wait')
+//     text.style.display = 'none'
+//     wait.style.display = 'inline'
+//     setTimeout(()=>{
+//         wait.style.display = 'none'
+//         text.textContent = 'Rendu Rastralaki Devudu'
+//         text.style.display = 'inline'
+//     }, 2000)
+// }
+ 
+// function time() {
+//     let mat1 = document.getElementById('mat1')
+//     let mat2 = document.getElementById('mat2')
+//     // let mat3 = document.getElementById('mat3')
+//     mat1.style.display = 'none'
+//     mat2.style.display = 'inline'
+//     setTimeout(()=>{
+//         mat2.style.display = 'none'
+//         mat1.textContent = 'Rendu Rastralaki Devudu'
+//         mat1.style.display = 'inline'
+//     },2000)
+// }
