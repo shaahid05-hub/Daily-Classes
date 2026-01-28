c0660a5007f1458b004772a0c892b3707ad3b9cf
Shaahid <mohammadshaahid05@gmail.com>
2026-01-28 13:58:30 +0530

    move

---
+++ JavaScript/DOM/Practice/app.js
@@ -0,0 +1,87 @@
+// let sel = document.querySelector('#a')
+// console.log(sel);
+
+// let btn = document.getElementById('btn')
+// let text = document.querySelector('#text')
+// btn.addEventListener('click',()=>{
+//     text.innerHTML = 'welcome'
+// }) 
+
+// let btn = document.getElementById('btn')
+
+// function change() {
+//     let text = document.getElementById('text')
+//     text.innerHTML='welcome'
+// }
+
+// document.getElementById('text').textContent = 'DOM'
+
+// 3rd
+// document.getElementById('a').style.backgroundColor = 'red'
+
+
+//4th
+// let btn = document.createElement('button')
+// btn.textContent = 'Click Me'
+// document.body.append(btn)
+
+// 5th
+// let box = document.createElement('div')
+// box.setAttribute('class', 'box')
+// document.body.append(box)
+// box.textContent = 'One'
+
+
+// 6th
+// let list = document.querySelectorAll('ul')
+// console.log(list);
+// list[0].style.color = 'red'
+
+//7th
+// function change() {
+//     let text = document.getElementById('text')
+//     text.textContent = 'Hello DOM'    
+// }
+
+//8th
+// function mouseE() {
+//     let box = document.getElementById('a')
+//     box.style.backgroundColor = 'red'
+// }
+// function mouseL() {
+//     let box = document.getElementById('a')
+//     box.style.backgroundColor = 'aqua'
+// }
+
+//9th
+// let text = document.getElementsByTagName('p')
+// for (let i = 0; i < text.length; i++) {
+//     text[i].style.color = 'red'
+// }
+
+//10th
+// let box = document.querySelector('.box')
+// box.style.borderWidth="3px"
+// box.style.borderStyle="solid"
+// box.style.borderColor="black"
+
+//11
+// function btn1() {
+//     let btn = document.getElementById('btn')
+//     btn.disabled = 'true'
+// }
+
+//12
+// let a = document.getElementById('btn')
+// let text = document.getElementById('text')
+// let count = Number(text.textContent)
+// a.addEventListener('click',()=>{
+//     count++
+//     text.textContent = count
+// })
+
+//12
+// function btn1() {
+//     let text = document.getElementById('text') 
+//     text.textContent = Number(text.textContent)+1 
+// }
