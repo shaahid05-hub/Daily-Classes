e969cf00c8d37fa19edb88d8c620db38c9ad078d
Shaahid <mohammadshaahid05@gmail.com>
2026-02-19 19:45:43 +0530

    Move

---
+++ JavaScript/Events/Music/app.js
@@ -0,0 +1,24 @@
+
+
+document.addEventListener('keydown',(e)=>{
+    switch (e.key.toLowerCase()) {
+        case 'a':
+            // console.log('you pressed ' + e.key);
+            document.getElementById('card1').style.transform = 'rotateY(45deg)'
+            break;
+        case 'b':
+            // console.log('you pressed ' +e.key);
+            break; 
+        case 'c':
+            // console.log('you pressed ' +e.key);
+            break;
+        case 'd':
+            // console.log('you pressed ' +e.key);
+            break;
+        case 'e':
+            // console.log('you pressed ' +e.key);
+            break;           
+        default:
+            break;
+    }
+})
